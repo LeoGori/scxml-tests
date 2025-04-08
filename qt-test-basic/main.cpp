308c05012211f6616eb95811a552f5e06e984834
Leonardo Gori <leonardo.gori@iit.it>
2025-04-08 13:10:55 +0200

    Edited code to test Dialog State Machine

--- qt-test-basic/main.cpp
+++ qt-test-basic/main.cpp
@@ -2,32 +2,63 @@
 #include <QScxmlStateMachine>
 #include <QDebug>
 
-
 #include <iostream>
-#include "MyDataModel.h"
-#include "MyStateMachine.h"
+// #include "MyDataModel.h"
+#include "build/MyStateMachine.h"
 
 #include <thread>
 #include <chrono>
 
-void myFunction(MyStateMachine *machine)
-{
-  int count=10;
-  while(--count) {
-    std::cout<<"Thread is running...\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+void myFunction(DialogSkillAction *machine) {
+  int count=2;
+  int num_machine_states = 2;
 
-    if (count%2==0)
-    {
-      machine->submitEvent("start", QVariantMap({
-        {"topic", "blah"}
-      }));
-    }  
-    else
-    {
-      machine->submitEvent("stop", QVariantMap({
-        {"topic", "asdf"}
-      }));
+  machine->connectToEvent("DialogComponent.waitForInteraction.Call", [machine]([[maybe_unused]]const QScxmlEvent & event){
+    QVariantMap data;
+    data.insert("result", "SUCCESS");
+    data.insert("interaction", "Helloo mamacita");
+    data.insert("topic", "Am I in waitForInteractResult state?");
+    machine->submitEvent("DialogComponent.waitForInteraction.Return", data);     
+  });
+
+  machine->connectToEvent("SchedulerComponent.getLanguage.Call", [machine]([[maybe_unused]]const QScxmlEvent & event){
+    QVariantMap data;
+    data.insert("result", "SUCCESS");
+    machine->submitEvent("SchedulerComponent.getLanguage.Return", data);     
+  });
+
+  machine->connectToEvent("DialogComponent.checkDuplicate.Call", [machine]([[maybe_unused]]const QScxmlEvent & event){
+    QVariantMap data;
+    data.insert("result", "SUCCESS");
+    data.insert("language", "eng");
+    machine->submitEvent("DialogComponent.checkDuplicate.Return", data);     
+  });
+
+  for (int i = 0; i < count; ++i) {
+    std::cout<<"Thread is running...\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+    int case_index = i % num_machine_states;
+
+    switch (case_index) {
+      case 0:
+        machine->submitEvent("CMD_TICK", QVariantMap({
+          {"topic", "Am I in waitForInteract state?"}
+        }));
+        break;
+      case 1:
+        machine->submitEvent("DialogComponent.waitForInteraction.Return", QVariantMap({
+          {"topic", "Am I in checkDuplicate state?"}
+        }));
+        break;
+      case 2:
+        machine->submitEvent("", QVariantMap({
+          {"topic", "Am I in waitForInteractionResult state?"}
+        }));
+        break;
+      default:
+        std::cout << "Invalid case index: " << case_index << std::endl;
+        break;
     }
   }
 
@@ -37,9 +68,9 @@ void myFunction(MyStateMachine *machine)
 int main(int argc, char *argv[])
 {
   QCoreApplication app(argc, argv);
-  MyDataModel dataModel;
-  MyStateMachine stateMachine;
-  stateMachine.setDataModel(&dataModel);
+  // MyDataModel dataModel;
+  DialogSkillAction stateMachine;
+  // stateMachine.setDataModel(&dataModel);
 
   stateMachine.start();
 
